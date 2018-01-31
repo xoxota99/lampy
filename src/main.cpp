48dde9f67cc21c0f06e7888862cfc901f9c85306
Phil Desrosiers <philippe.desrosiers@gmail.com>
2018-01-31 00:08:51 -0800

    Fixed PANEL_COUNT

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <SPI.h>
 
 #define PIXELS_PER_PANEL 64
-#define PANEL_COUNT 64
+#define PANEL_COUNT 6
 const int NUMPIXELS = PIXELS_PER_PANEL * PANEL_COUNT;
 
 // Constructor for hardware SPI -- must connect to MOSI, SCK pins
@@ -13,13 +13,15 @@ void setup() {
   //power indicator
   pinMode(LED_BUILTIN,OUTPUT);
   digitalWrite(LED_BUILTIN,HIGH);
-  
+
   strip.begin(); // Initialize pins for output
   strip.show();  // Turn all LEDs off ASAP
 }
 
 // Runs 10 LEDs at a time along strip, cycling through red, green and blue.
-// This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+// This requires about 40 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+// in a cube of 384 LEDs, this is about 16 amps. Doble check your batteries
+// C-Rating, kids!
 
 int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
 uint32_t color = 0xFF0000;      // 'On' color (starts red)
