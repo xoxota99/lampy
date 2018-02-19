ac62348c8d7ba92cff452adff0df047aa3dcf9d0
Phil <philippe.desrosiers@gmail.com>
2018-02-19 14:58:50 -0800

    No physical inputs. Fxed to work properly with Dostar matrices in a cube form.

--- src/main.cpp
+++ src/main.cpp
@@ -23,12 +23,6 @@ const int _GBASE_ = __COUNTER__ + 1; // Index of 1st __COUNTER__ ref below
 #define _G3_ _G2_ _G2_ _G2_ _G2_ _G2_ _G2_ _G2_ _G2_ // Expands to 64 items
 const uint8_t PROGMEM gammaTable[] = { _G3_ _G3_ _G3_ _G3_ }; // 256 items
 
-// These are used for 'debouncing' the left & right button inputs,
-// for switching between modes.
-// boolean  prevStateLeft, prevStateRight;
-// uint32_t lastChangeTimeLeft = 0, lastChangeTimeRight = 0;
-// #define  DEBOUNCE_MS 15 // Button debounce time, in milliseconds
-
 // These are used in estimating (very approximately) the current draw of
 // the board and NeoPixels.  BASECURRENT is the MINIMUM current (in mA)
 // used by the entire system (microcontroller board plus NeoPixels) --
@@ -40,9 +34,9 @@ const uint8_t PROGMEM gammaTable[] = { _G3_ _G3_ _G3_ _G3_ }; // 256 items
 // common '5050' type used in NeoPixel strips and shapes.
 #define BASECURRENT 10
 #define LEDCURRENT  13.3 // Try using 18 for '5050' NeoPixels
-#define BRIGHTNESS  32
+#define BRIGHTNESS  16
 uint8_t  frame = 0;    // Frame count, results displayed every 256 frames
-uint32_t sum   = 0;    // Cumulative current, for calculating average
+long int sum   = 0;    // Cumulative current, for calculating average
 uint8_t *pixelPtr;     // -> NeoPixel color data
 
 void mode_off();
@@ -65,7 +59,7 @@ void mode_sine_half_gamma();
 // we return to "mode_off" at several points in the sequence.
 void (*renderFunc[])(void) {
   mode_off, // Starts here, with LEDs off
-  mode_white_max, mode_white_half_duty , mode_white_half_perceptual, mode_off,
+  mode_white_max, mode_white_half_duty , mode_white_half_perceptual,
   mode_primaries, mode_colorwheel, mode_colorwheel_gamma,
   mode_half, mode_sparkle,
   mode_marquee, mode_sine, mode_sine_gamma, mode_sine_half_gamma
@@ -73,15 +67,20 @@ void (*renderFunc[])(void) {
 const int N_MODES = (sizeof(renderFunc) / sizeof(renderFunc[0]));
 uint8_t mode = 0; // Index of current mode in table
 long int lastChangeTime;
-#define MODE_CHANGE_TIME_MS 5000    //milliseconds between mode changes.
+#define MODE_CHANGE_TIME_MS 2500    //milliseconds between mode changes.
 
+//Hardware SPI constructor, for tiled matrices.
 Adafruit_DotStarMatrix matrix = Adafruit_DotStarMatrix(
- 8, 48,
- DS_MATRIX_TOP     + DS_MATRIX_LEFT +
- DS_MATRIX_COLUMNS + DS_MATRIX_PROGRESSIVE,
- DOTSTAR_GRB);
+(uint8_t)8, (uint8_t)8, 6, 1,
+DS_TILE_TOP   + DS_TILE_LEFT   + DS_TILE_ROWS      + DS_TILE_PROGRESSIVE +
+DS_MATRIX_TOP + DS_MATRIX_LEFT + DS_MATRIX_COLUMNS + DS_MATRIX_PROGRESSIVE,
+DOTSTAR_GRB);
 
-
+// Adafruit_DotStarMatrix matrix = Adafruit_DotStarMatrix(
+// 8, 8,
+// DS_MATRIX_TOP     + DS_MATRIX_LEFT +
+// DS_MATRIX_COLUMNS + DS_MATRIX_PROGRESSIVE,
+// DOTSTAR_GRB);
 // SETUP FUNCTION -- RUNS ONCE AT PROGRAM START ----------------------------
 
 void setup() {
@@ -91,40 +90,19 @@ void setup() {
 
   Serial.begin(19200);
 
-  // prevStateLeft  = matrix.leftButton(); // Initial button states
-  // prevStateRight = matrix.rightButton();
+// mode_off();
+// matrix.show();
+// delay(1000);
+
+  // mode=6;   //colorwheel_gamma
+
 }
 
 // LOOP FUNCTION -- RUNS OVER AND OVER FOREVER -----------------------------
 
 void loop() {
 
-  // Read and debounce left/right buttons
   uint32_t t = millis();
-  // if((t - lastChangeTimeLeft) >= DEBOUNCE_MS) {
-  //   boolean b = matrix.leftButton();
-  //   if(b != prevStateLeft) {             // Left button state changed?
-  //     prevStateLeft      = b;
-  //     lastChangeTimeLeft = t;
-  //     if(b) {                            // Left button pressed?
-  //       if(mode) mode--;                 // Go to prior mode
-  //       else     mode = N_MODES - 1;     // or "wrap around" to last mode
-  //       frame = sum = 0;                 // Reset power calculation
-  //     }
-  //   }
-  // }
-  // if((t - lastChangeTimeRight) >= DEBOUNCE_MS) {
-  //   boolean b = matrix.rightButton();
-  //   if(b != prevStateRight) {            // Right button state changed?
-  //     prevStateRight      = b;
-  //     lastChangeTimeRight = t;
-  //     if(b) {                            // Right button pressed?
-  //       if(mode < (N_MODES-1)) mode++;   // Advance to next mode
-  //       else                   mode = 0; // or "wrap around" to start
-  //       frame = sum = 0;                 // Reset power calc
-  //     }
-  //   }
-  // }
 
   //for now, just change modes every ten seconds or something.
   if((t-lastChangeTime) >= MODE_CHANGE_TIME_MS){
@@ -133,14 +111,15 @@ void loop() {
   }
 
   (*renderFunc[mode])();          // Render one frame in current mode
-  matrix.show(); // and update the NeoPixels to show it
+  matrix.show();                  // and update the NeoPixels to show it
 
   // Accumulate total brightness value for all NeoPixels (assumes RGB).
-  for(uint8_t i=0; i<matrix.numPixels() * 3; i++) {
+  for(int i=0; i<matrix.numPixels() * 3; i++) {
     sum += pixelPtr[i];
   }
   if(!++frame) { // Every 256th frame, estimate & print current
-    Serial.print(BASECURRENT + (sum * (LEDCURRENT * BRIGHTNESS / 256) + 32640) / 65280);
+    long int mA=BASECURRENT + (sum * (LEDCURRENT * BRIGHTNESS / 256) + 32640) / 65280;
+    Serial.print(mA);
     Serial.println(" mA");
     sum = 0; // Reset pixel accumulator
   }
@@ -155,7 +134,7 @@ void mode_off() {
 
 // All NeoPixels on at max: white (R+G+B) at 100% duty cycle
 void mode_white_max() {
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     matrix.setPixelColor(i, 0xFFFFFF);
   }
 }
@@ -163,7 +142,7 @@ void mode_white_max() {
 // All NeoPixels on at 50% duty cycle white.  Numerically speaking,
 // this is half power, but perceptually it appears brighter than 50%.
 void mode_white_half_duty() {
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     matrix.setPixelColor(i, 0x7F7F7F);
   }
 }
@@ -174,7 +153,7 @@ void mode_white_half_duty() {
 // can actually be using 1/5 the power!
 void mode_white_half_perceptual() {
   uint32_t c = pgm_read_byte(&gammaTable[127]) * 0x010101;
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     matrix.setPixelColor(i, c);
   }
 }
@@ -184,7 +163,7 @@ void mode_white_half_perceptual() {
 // at any given time, this uses 1/3 the power of the "white max" mode.
 void mode_primaries() {
   uint32_t c;
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     // This animation (and many of the rest) pretend spatially that there's
     // 12 equally-spaced NeoPixels, though in reality there's only 10 with
     // gaps at the USB and battery connectors.
@@ -233,7 +212,7 @@ uint32_t hsv2rgb(int32_t h, uint8_t s, uint8_t v, boolean gc=false) {
 // Average current use is about 1/2 of the max-all-white case.
 void mode_colorwheel() {
   uint32_t t = millis();
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     uint8_t j = i + (i > 4);
     matrix.setPixelColor(i,
       hsv2rgb(t + j * 1530 / 12, 255, 255, false));
@@ -245,7 +224,7 @@ void mode_colorwheel() {
 // any time are in transition cases (else 100% on or off).
 void mode_colorwheel_gamma() {
   uint32_t t = millis();
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     uint8_t j = i + (i > 4);
     matrix.setPixelColor(i,
       hsv2rgb(t + j * 1530 / 12, 255, 255, true));
@@ -256,21 +235,21 @@ void mode_colorwheel_gamma() {
 // Simple idea.  Half the pixels means half the power use.
 void mode_half() {
   uint32_t t = millis() / 4;
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     uint8_t j = t + i * 256 / matrix.numPixels();
     j = (j >> 7) * 255;
     matrix.setPixelColor(i, j * 0x010000);
   }
 }
 
-// Blue sparkles.  Randomly turns on ONE pixel at a time.  This demonstrates
+// Random sparkles.  Randomly turns on ONE pixel at a time.  This demonstrates
 // minimal power use while still doing something "catchy."  And because it's
 // a primary color, power use is even minimal-er (see 'primaries' above).
 void mode_sparkle() {
-  static uint8_t randomPixel = 0;
-  if(!(frame & 0x7F)) {              // Every 128 frames...
+  static int randomPixel = 0;
+  if(!(frame & 0x02)) {              // Every 2 frames...
     matrix.clear(); // Clear pixels
-    uint8_t r;
+    int r;
     do {
       r = random(matrix.numPixels());                // Pick a new random pixel
     } while(r == randomPixel);       // but not the same as last time
@@ -285,7 +264,7 @@ void mode_sparkle() {
 // transition into the examples that follow.
 void mode_marquee() {
   uint32_t t = millis() / 4;
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     uint8_t j = i + (i > 4);
     uint8_t k = (t + j * 256 / 12) & 0xFF;
     k = ((k >> 6) & 1) * 255;
@@ -297,7 +276,7 @@ void mode_marquee() {
 // and combined with being a primary color, uses about 1/6 the max current.
 void mode_sine() {
   uint32_t t = millis() / 4;
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     uint8_t j = i + (i > 4);
     uint8_t k = pgm_read_byte(&sineTable[(t + j * 512 / 12) & 0xFF]);
     matrix.setPixelColor(i, k * 0x000100L);
@@ -309,7 +288,7 @@ void mode_sine() {
 // savings to gamma correction, in addition to looking more "correct."
 void mode_sine_gamma() {
   uint32_t t = millis() / 4;
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     uint8_t j = i + (i > 4);
     uint8_t k = pgm_read_byte(&sineTable[(t + j * 512 / 12) & 0xFF]);
     k = pgm_read_byte(&gammaTable[k]);
@@ -322,7 +301,7 @@ void mode_sine_gamma() {
 // gamma and primary color use, it's super effective!
 void mode_sine_half_gamma() {
   uint32_t t = millis() / 4;
-  for(uint8_t i=0; i<matrix.numPixels(); i++) {
+  for(int i=0; i<matrix.numPixels(); i++) {
     uint8_t j = i + (i > 4);
     uint8_t k = pgm_read_byte(&sineTable[(t + j * 512 / 12) & 0xFF]) / 2;
     k = pgm_read_byte(&gammaTable[k]);
